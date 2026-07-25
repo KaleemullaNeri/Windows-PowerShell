0951721422d4c9d175fb9997e6071db43db85431
KaleemullaNeri <kaleemullaneri@outlook.com>
2026-07-25 17:28:00 +0530

    Add files via upload
    Window Device BitLocker encryption script

---
+++ BitLocker PowerShell Script/Bitlocker Script3.ps1
@@ -0,0 +1,57 @@
+﻿try {
+
+    $TPM = Get-Tpm
+
+    if (($TPM.TpmPresent -ne $true) -or ($TPM.TpmReady -ne $true)) {
+        Write-Output "TPM not ready."
+        exit 0
+    }
+
+    # Process C Drive
+    $CDrive = Get-BitLockerVolume -MountPoint "C:"
+
+    if ($CDrive.ProtectionStatus -ne "On") {
+
+        Write-Output "Encrypting C:"
+
+        Enable-BitLocker `
+            -MountPoint "C:" `
+            -EncryptionMethod XtsAes256 `
+            -UsedSpaceOnly `
+            -TpmProtector `
+            -SkipHardwareTest
+    }
+    else {
+        Write-Output "C: already encrypted."
+    }
+
+    # Process D Drive if available
+    if (Test-Path "D:\") {
+
+        $DDrive = Get-BitLockerVolume -MountPoint "D:"
+
+        if ($DDrive.ProtectionStatus -ne "On") {
+
+            Write-Output "Encrypting D:"
+
+            Enable-BitLocker `
+                -MountPoint "D:" `
+                -EncryptionMethod XtsAes256 `
+                -UsedSpaceOnly `
+                -RecoveryPasswordProtector `
+                -SkipHardwareTest
+        }
+        else {
+            Write-Output "D: already encrypted."
+        }
+    }
+    else {
+        Write-Output "D: drive not present."
+    }
+
+    exit 0
+}
+catch {
+    Write-Output $_.Exception.Message
+    exit 0
+}
